391721d014c2efc73a8a764194b7a7bd1ea1e361
Renan Gomes Acosta <renan.acosta@compasso.com.br>
2023-08-04 00:46:22 -0300

    :lipstick: feat: Dark mode adjust css style

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,11 +1,12 @@
 <template>
   <header>
-    <h1>
+    <h1 class="image">
       <img src="../assets/logo.png" alt="Logo" />
     </h1>
     <button class="button" @click="alterarTema">
       {{ textoBotao }}
     </button>
+    <p class="author">By Renan Acosta</p>
   </header>
 </template>
 
@@ -38,13 +39,32 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
+.image {
+margin-bottom: 1rem;
+}
+
+.button {
+  background: #fcba03;
+  color: #5c5a5a;
+  font-weight: bold;
+  border: none;
+  margin-bottom: 1rem;
+}
+
 header {
   padding: 1rem;
-  background: #0d3b66;
+  background: #222;
   width: 100%;
   height: 100vh;
   text-align: center;
+}
+.author {
+  color: #fff;
+  font-size: 1rem;
+  margin-top: 1rem;
+  border: 1px solid #fff;
+  border-radius: 5px;
 }
 @media only screen and (max-width: 768px) {
   header {
