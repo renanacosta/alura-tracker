319ca738807859045b486e66b572bd1100d9f0f8
Renan Gomes Acosta <renan.acosta@compasso.com.br>
2023-08-03 23:41:05 -0300

    :construction: feat: Renderização condicional

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
       <Formulario @aoSalvarTarefa="salvarTarefa"/>
       <div class="lista">
         <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+      <Box v-if="listaEstaVazia">
+        Você não está muito produtivo hoje :(
+      </Box>
       </div>
     </div>
   </main>
@@ -17,6 +20,7 @@ import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
 import Formulario from "./components/Formulario.vue";
 import Tarefa from "./components/Tarefa.vue";
+import Box from "./components/Box.vue";
 import ITarefa  from "./interfaces/ITarefa";
 
 export default defineComponent({
@@ -25,11 +29,17 @@ export default defineComponent({
     BarraLateral,
     Formulario,
     Tarefa,
+    Box,
   },
   data() {
     return {
       tarefas: [] as ITarefa[],
     };
+  },
+  computed: {
+    listaEstaVazia (): boolean {
+      return this.tarefas.length === 0;
+    },
   },
   methods: {
     salvarTarefa (tarefa: ITarefa) {
