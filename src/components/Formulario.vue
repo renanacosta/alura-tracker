a454e14721e4a9f6756f1ce4923737f879494481
Renan Gomes Acosta <renan.acosta@compasso.com.br>
2023-08-03 21:57:37 -0300

    :construction: feat: cronometrando tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,15 +17,15 @@
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>Play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -42,6 +42,29 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Formulário",
+  data() {
+    return {
+        tempoEmSegundos: 0
+    };
+  },
+  computed: {
+    tempoDecorrido () : string {
+        return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+    }
+  },
+  methods: {
+    iniciar() {
+        // começar contagem
+        // 1 seg = 1000 milisegundos
+      setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+        // finalizar contagem
+        this.tempoEmSegundos = 0;
+    },
+  },
 });
 </script>
 
