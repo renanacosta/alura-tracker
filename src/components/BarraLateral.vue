d03934276f9024d40f06372f521fa7027a0d2763
Renan Gomes Acosta <renan.acosta@compasso.com.br>
2023-08-04 18:32:05 -0300

    :recycle: refactor: Cadastrando projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -3,10 +3,30 @@
     <h1 class="image">
       <img src="../assets/logo.png" alt="Logo" />
     </h1>
-    <button class="button" @click="alterarTema">
-      {{ textoBotao }}
-    </button>
-    <p class="author">By Renan Acosta</p>
+    <div class="has-text-centered">
+      <button class="button" @click="alterarTema">
+        {{ textoBotao }}
+      </button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="author">
+    <p>By Renan Acosta</p>
+    </div>
   </header>
 </template>
 
@@ -27,7 +47,7 @@ export default defineComponent({
         return "Desativar modo escuro";
       } else {
         return "Ativar modo escuro";
-    }
+      }
     },
   },
   methods: {
@@ -41,7 +61,7 @@ export default defineComponent({
 
 <style scoped>
 .image {
-margin-bottom: 1rem;
+  margin-bottom: 1rem;
 }
 
 .button {
@@ -57,7 +77,6 @@ header {
   background: #222;
   width: 100%;
   height: 100vh;
-  text-align: center;
 }
 .author {
   color: #fff;
@@ -65,11 +84,25 @@ header {
   margin-top: 1rem;
   border: 1px solid #fff;
   border-radius: 5px;
+  text-align: center;
 }
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #faf0ca;
+}
+.link.router-link-active {
+  color: #faf0ca;
 }
 </style>
