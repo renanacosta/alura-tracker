bbba49fcffb33a8fd5c89d0a1c4cbbf89b463eeb
Renan Gomes Acosta <renan.acosta@compasso.com.br>
2023-08-07 14:27:16 -0300

    :refactor: refact: Rotas aninhadas

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from "../views/Tarefas.vue";
 import Projetos from "../views/Projetos.vue";
+import Lista from "../views/Projetos/Lista.vue";
 import Formulario from "../views/Projetos/Formulario.vue";
 
 const rotas: RouteRecordRaw[] = [
@@ -11,20 +12,26 @@ const rotas: RouteRecordRaw[] = [
   },
   {
     path: "/projetos",
-    name: "Projetos",
     component: Projetos,
-  },
-  {
-    path: "/projetos/novo",
-    name: "Novo projetos",
-    component: Formulario,
-  },
-  {
-    path: "/projetos/:id",
-    name: "Editar projetos",
-    component: Formulario,
-    props: true,
-  },
+    children: [
+      {
+        path: "",
+        name: "Projetos",
+        component: Lista
+      },
+      {
+        path: "novo",
+        name: "Novo projeto",
+        component: Formulario,
+      },
+      {
+        path: ":id",
+        name: "Editar projeto",
+        component: Formulario,
+        props: true,
+      },
+    ]
+  }
 ];
 
 const roteador = createRouter({
