a8372e88099e4c4dc398869967213ac0f2bc0223
Renan Gomes Acosta <renan.acosta@compasso.com.br>
2023-08-07 14:09:11 -0300

    :refactor: refact: Refatorando as views de edição do projeto

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,26 +3,32 @@ import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { InjectionKey } from "vue";
 
 interface Estado {
-    projetos: IProjeto[];
+  projetos: IProjeto[];
 }
 
-export const key: InjectionKey<Store<Estado>> = Symbol()
+export const key: InjectionKey<Store<Estado>> = Symbol();
 
 export const store = createStore<Estado>({
-    state: {
-        projetos: []
+  state: {
+    projetos: [],
+  },
+  mutations: {
+    ADICIONA_PROJETO(state, nomeDoProjeto: string) {
+      const projeto = {
+        id: new Date().toISOString(),
+        nome: nomeDoProjeto,
+      } as IProjeto;
+      state.projetos.push(projeto);
     },
-    mutations: {
-        'ADICIONA_PROJETO' (state, nomeDoProjeto: string) {
-            const projeto = {
-                id: new Date().toISOString(),
-                nome: nomeDoProjeto,
-            } as IProjeto;
-            state.projetos.push(projeto);
-        }
-    }
+    ALTERA_PROJETO(state, projeto: IProjeto) {
+      const index = state.projetos.findIndex(
+        (projeto) => projeto.id === projeto.id
+      );
+      state.projetos[index] = projeto;
+    },
+  },
 });
 
 export function useStore(): Store<Estado> {
-    return vuexUseStore(key);
+  return vuexUseStore(key);
 }
